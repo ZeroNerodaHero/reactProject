8857a6493cde1ee8abe3ba03af36c27d21dbeca2
ZeroNerodaHero <billyz404meter@gmail.com>
2022-12-26 21:23:16 -0800

    Add files via upload

---
+++ installation.sql
@@ -0,0 +1,32 @@
+CREATE TABLE userList(
+    userId int,
+    sessionId int,
+    sessionExpire timeStamp,
+    first_name varchar(60),
+    last_name varchar(60),
+    
+    username varchar(60),
+    password varchar(60),
+    email varchar(160),
+    phoneNum varchar(32),
+    
+    accountPerm int,
+    
+    currentRequestId int,
+    status int
+)
+/*
+INSERT INTO userList(userId,first_name,last_name,username,password,email,phoneNum,accountPerm)
+    VALUES(123456,"TEST","TEST","TEST","TEST","TEST@TEST.TEST","123-567-9012",0);
+*/
+CREATE TABLE history_123456(
+    requestId int,
+    time timestamp,
+    location_x double,
+    location_y double,
+    feature varchar(800),
+    meet_up varchar(800),
+    emergency bool,
+    additional_info varchar(800) 
+)
+    
